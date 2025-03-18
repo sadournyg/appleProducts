55c1ecaff8a15d90074b3f87b287e29537033340
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-03-18 09:12:22 +0100

    Frame creation: added a resizable centered container

--- apple-products-store/src/App.jsx
+++ apple-products-store/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <div className="w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto"></div>
+    </div>
+  );
 };
 
 export default App;
