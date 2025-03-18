fa2ab24c8780b452c4bfc8ba05e61b349ac6e256
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-03-18 10:19:58 +0100

    Navbar: Added sidebar navigation with active state

--- apple-products-store/src/App.jsx
+++ apple-products-store/src/App.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import Controls from "./components/Controls";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false);
+  const [activePage, setActivePage] = useState(0);
+
+  const handleNavClick = (pageIndex) => {
+    setActivePage(pageIndex);
+  };
 
   const toggleZoom = () => {
     setFrameZoom(!frameZoom);
@@ -14,6 +20,7 @@ const App = () => {
           frameZoom && "min-w-[97vw] min-h-[97vh]"
         } w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}
       >
+        <Navbar activePage={activePage} handleNavClick={handleNavClick} />
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
       </div>
     </div>
